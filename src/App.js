7d677381f3bf2d57cd8d8845a4fcbccb0f276d6f
superbug <sunweijun1989@gmail.com>
2017-08-16 16:13:56 +0800

    format code

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return(
-     <div className="App">
+    return (
+      <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
