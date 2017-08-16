2171c2d3ebb3d0ae1e6d2098d0cb0fb01270a5b1
superbug <sunweijun1989@gmail.com>
2017-08-16 21:45:32 +0800

    use react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import About from './About';
+import Todo from './Todo';
+import Repos from './Repos';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={App} />
+      <Route path="/repos" component={Repos} />
+      <Route path="/todo" component={Todo} />
+      <Route path="/about" component={About} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
