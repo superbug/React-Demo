dc165a3e93182a9a9b4a137db78e259d75cf2401
superbug <sunweijun1989@gmail.com>
2017-08-17 17:01:30 +0800

    loading..

--- src/Repos.js
+++ src/Repos.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Switch, Route } from 'react-router';
+import { Layout, Menu, Spin } from 'antd';
 import Repo from './Repo';
+const { Header, Content, Sider } = Layout;
 
 class Repos extends Component {
   state = {
@@ -19,22 +21,31 @@ class Repos extends Component {
   render() {
     const { repos } = this.state;
     return (
-      <div>
-        <ul>
-          {repos
-            ? repos.map((repo, index) =>
-                <li key={index}>
-                  <NavLink to={`/repos/${repo.name}`}>
-                    {repo.name}
-                  </NavLink>
-                </li>
-              )
-            : <div>loading...</div>}
-        </ul>
-        <Switch>
-          <Route path="/repos/:repoName" component={Repo} />
-        </Switch>
-      </div>
+      <Layout style={{ background: '#fff' }}>
+        <Sider width={200} style={{ background: '#fff' }}>
+          {repos ? null : <Spin />}
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['0']}
+            style={{ height: '100%' }}
+          >
+            {repos
+              ? repos.map((repo, index) =>
+                  <Menu.Item key={index}>
+                    <NavLink to={`/repos/${repo.name}`}>
+                      {repo.name}
+                    </NavLink>
+                  </Menu.Item>
+                )
+              : null}
+          </Menu>
+        </Sider>
+        <Content style={{ padding: '0 24px', minHeight: 280 }}>
+          <Switch>
+            <Route path="/repos/:repoName" component={Repo} />
+          </Switch>
+        </Content>
+      </Layout>
     );
   }
 }
